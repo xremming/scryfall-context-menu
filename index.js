780e2e2b43ae743c7770a9a34006260d63e0c697
Maximilian Remming <maxremming@gmail.com>
2019-01-04 22:01:09 +0200

    Cleanup and fix onclick handler

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var contextMenu = chrome.contextMenus.create({
-  title: "Search with Scryfall",
-  contexts: ["selection"]
-});
-
-contextMenu.onClicked.addListener(function(info, tab) {
-  var t = info.selectionText;
-  if (t === null || t === undefined || t.length == 0) return;
-
+function search(info, tab) {
   chrome.tabs.create({
-    url: `https://scryfall.com/search?q=${encodeURIComponent(t)}`,
+    url: "https://scryfall.com/search?q=" + encodeURIComponent(info.selectionText),
     active: true
   });
+}
+
+chrome.contextMenus.create({
+  title: 'Search Scryfall for "%s"',
+  contexts: ["selection"],
+  onclick: search
 });
